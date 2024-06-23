7875cb63959c237f11c52a4b91a38ade257af444
Tobias Ritter <tobias.ritter98@gmail.com>
2024-06-23 10:24:38 +0200

    update

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 import numpy as np
+import plotly.express as px
 
 st.markdown("# Normal Distribution Sample Generator")
 
 size = st.slider("Sample size", 100, 1000)
 
 arr = np.random.normal(1, 1, size=size)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
+fig = px.histogram(arr)
 
-st.pyplot(fig)
+st.plotly_chart(fig)
