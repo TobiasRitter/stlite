5bdca45fd96155d8a10467f80527deee09443e81
Tobias Ritter <tobias.ritter98@gmail.com>
2024-06-23 10:11:49 +0200

    update

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+
+st.markdown("# Normal Distribution Sample Generator")
+
+size = st.slider("Sample size", 100, 1000)
+
+arr = np.random.normal(1, 1, size=size)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
