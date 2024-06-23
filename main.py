2a27744b62fcd6f29df026d5cbec6667d60f52f5
Tobias Ritter <tobias.ritter98@gmail.com>
2024-06-23 10:57:53 +0200

    update

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ st.markdown("# Normal Distribution Sample Generator")
 size = st.slider("Sample size", 100, 1000)
 
 arr = np.random.normal(1, 1, size=size)
-fig = px.histogram(arr)
 
-st.plotly_chart(fig)
+hist = px.histogram(arr)
+st.plotly_chart(hist)
